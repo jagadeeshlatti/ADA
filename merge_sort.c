0e4d701ff7a8c67c1683d5aa3681a36349257290
JAGADEESH A LATTI <117442431+jagadeeshlatti@users.noreply.github.com>
2023-07-22 12:10:23 +0530

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,77 +1,82 @@
 #include <stdio.h>
-int c[50];
+#include <stdlib.h>
+#include <time.h>
 
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-int main() {
-    int size;
-    printf("Enter the number of elements: ");
-    scanf("%d", &size);
+    int L[n1], R[n2];
 
-    int arr[size];
-    printf("Enter the elements:\n");
-    for (int i = 0; i < size; i++)
-        scanf("%d", &arr[i]);
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-    printf("Original array: ");
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    i = 0;
+    j = 0;
+    k = l;
 
-
-    mergeSort(arr, 0, size - 1);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-
-
-    return 0;
-}
-
-void mergeSort(int arr[], int low, int high) {
-    if (low < high) {
-        int mid = (low+high) / 2;
-
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-
-        merge(arr, low, mid, high);
-    }
-}
-
-
-void merge(int arr[], int low, int mid, int high) {
-    int i= low ;
-    int j =  mid+1;
-    int k=0;
-
-    while (i <=mid  && j <=high) {
-        if (arr[i] <= arr[j]) {
-            c[k] = arr[i];
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-            k++;
         } else {
-            c[k] = arr[j];
+            arr[k] = R[j];
             j++;
-            k++;
         }
-
+        k++;
     }
 
-    while (i <=mid) {
-        c[k] = arr[i];
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
         k++;
     }
 
-
-    while (j <= high) {
-        c[k] = arr[j];
+    while (j < n2) {
+        arr[k] = R[j];
         j++;
         k++;
     }
+}
 
-    for(k = 0 ; k < (high-low+1) ; k++)
-        arr[low + k] = c[k];
+void merge_sort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements to sort: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        arr[i]=rand();
+        //scanf("%d", &arr[i]);
+    }
+
+    clock_t start = clock();
+    merge_sort(arr, 0, n - 1);
+    clock_t end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\nTime taken to sort %d elements: %f seconds\n", n, time_taken);
+
+    return 0;
 }
